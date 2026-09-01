9cb39240430d2509d7bc7a0638316118addacc4d
santiagomadrigal <barucmadrigal@ciencias.unam.mx>
2026-09-01 13:14:17 -0600

    Meti las practicas en carpetas

---
+++ Practica1/practica_1.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Practica1{
+    private int k;
+    private String v; 
+    Arrays 
+    LinkedList cubetas = new LinkedList();
+
+
+    private int dispersion(k){
+        return k % 7;
+
+    }
+    private int factorCarga(){
+
+    }
+}
